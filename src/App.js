68b928f881e0697e1444d2d18da4e0a9a0370593
parockson <parockson@gmail.com>
2026-02-10 16:16:44 +0000

    fix unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './styles/main.scss';
 import Header from './components/Header';
 import Introduction from './components/Introduction';
