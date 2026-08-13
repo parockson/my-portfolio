4d132e574127bfd38cc2ffd76ab15967459d8855
parockson <parockson@gmail.com>
2026-08-13 22:08:59 +0000

    update

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,7 +4,7 @@ const Introduction = () => {
     return (
         <section className="portfolio-section">
             <h2>Profile</h2>
-            <p>Prince Rockson is a Biomedical Data Scientist and Data Analyst with a multidisciplinary background in health informatics, molecular biotechnology, computational biology, and software engineering. He specialises in machine learning, data analytics, ETL, and BI, with research focused on developing robust and interpretable AI systems for healthcare, particularly chronic disease prediction and staging. He is passionate about applying computational approaches to improve early disease detection and healthcare decision-making, with aspirations to pursue a PhD in Biomedical Data Science, Biostatistics, or Health Informatics.</p>
+            <p>I am a Biomedical Data Scientist and Data Analyst with a strong foundation in health informatics, molecular biotechnology, computational biology, and software engineering. I specialise in machine learning, data analytics, ETL, and BI, with research focused on developing robust and interpretable AI systems for healthcare, particularly chronic disease prediction and staging. I am passionate about applying computational approaches to improve early disease detection and healthcare decision-making, with aspirations to pursue a PhD in Biomedical Data Science, Biostatistics, or Health Informatics.</p>
         </section>
     );
 };
